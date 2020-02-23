a05c29f610328dfeacdc9b247a009d88b08b2d32
Paweł Kolbiarz <pawel.kolbiarz@ebicom.pl>
2020-02-23 13:02:14 +0100

    Added test project. Added first test and controller to manage characters.

---
+++ StarWars.Tests/StarWars.API.Tests/CharacterControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using StarWars.API.Controllers;
+using Xunit;
+
+namespace StarWars.Tests.StarWars.API.Tests
+{
+	public class CharacterControllerTests
+	{
+		[Fact]
+		public void CharacterController_Should_Exists()
+		{
+			var controller = new CharacterController();
+
+			Assert.NotNull(controller);
+		}
+	}
+}
