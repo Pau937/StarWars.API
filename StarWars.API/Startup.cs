6960812fb4ec4bf5c1c93e480df3d91f895039ef
Paweł Kolbiarz <pawel.kolbiarz@gmail.com>
2020-10-30 20:25:30 +0100

    Test

--- StarWars.API/Startup.cs
+++ StarWars.API/Startup.cs
@@ -22,107 +22,108 @@ using System.Text;
 
 namespace StarWars.API
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		public void ConfigureTestingServices(IServiceCollection services)
-		{
-			services.AddDbContext<DataContext>(options =>
-				options.UseInMemoryDatabase("DatabaseInMemoryForFunctionalTests"));
+        public void ConfigureTestingServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+                options.UseInMemoryDatabase("DatabaseInMemoryForFunctionalTests"));
 
-			ConfigureServices(services);
-		}
+            ConfigureServices(services);
+        }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			var mappingConfig = new MapperConfiguration(conf =>
-			{
-				conf.AddProfile(new MappingProfile());
-			});
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var mappingConfig = new MapperConfiguration(conf =>
+            {
+                conf.AddProfile(new MappingProfile());
+            });
 
-			IMapper mapper = mappingConfig.CreateMapper();
+            IMapper mapper = mappingConfig.CreateMapper();
 
-			services.AddControllers();
-			services.AddSingleton(mapper);
-			services.AddScoped(typeof(IAsyncRepository<>), typeof(EFCoreRepository<>));
-			services.AddScoped<IAsyncRepository<Character>, CharacterRepository>(); 
-			services.AddScoped<ICharacterService, CharacterService>();
-			services.AddScoped<IPlanetService, PlanetService>();
-			services.AddScoped<IEpisodeService, EpisodeService>();
-			services.AddScoped<IFriendshipService, FriendshipService>();
-			services.AddScoped<IAuthenticationService, AuthenticationService>();
-			services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuerSigningKey = true,
-					IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:SecretToken").Value)),
-					ValidateIssuer = false,
-					ValidateAudience = false
-				});
+            services.AddControllers();
+            services.AddSingleton(mapper);
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(EFCoreRepository<>));
+            services.AddScoped<IAsyncRepository<Character>, CharacterRepository>();
+            services.AddScoped<ICharacterService, CharacterService>();
+            services.AddScoped<IPlanetService, PlanetService>();
+            services.AddScoped<IEpisodeService, EpisodeService>();
+            services.AddScoped<IFriendshipService, FriendshipService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:SecretToken").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                });
 
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo
-				{
-					Version = "v1",
-					Title = "StarWars.API",
-					Description = "Manage your characters now!",
-				});
-			});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "StarWars.API",
+                    Description = "Manage your characters now!",
+                });
+            });
 
-			services.AddMvc()
-			 .AddJsonOptions(options => {
-				 options.JsonSerializerOptions.IgnoreNullValues = true;
-			 });
-		}
+            services.AddMvc()
+             .AddJsonOptions(options =>
+             {
+                 options.JsonSerializerOptions.IgnoreNullValues = true;
+             });
+        }
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler(builder => builder.Run(async context =>
-				{
-					context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder => builder.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-					var error = context.Features.Get<IExceptionHandlerFeature>();
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
 
-					if (error != null)
-					{
-						await context.Response.WriteAsync(error.Error.Message);
-					}
-				}));
-			}
+                    if (error != null)
+                    {
+                        await context.Response.WriteAsync(error.Error.Message);
+                    }
+                }));
+            }
 
-			app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
-			app.UseSwagger();
+            app.UseSwagger();
 
-			app.UseSwaggerUI(c =>
-			{
-				c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarWars.API");
-				c.RoutePrefix = string.Empty;
-			});
-		}
-	}
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarWars.API");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
 }
